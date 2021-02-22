b18b8dfcc11845841fc96eb2dd5895ae93232aea
Matt Mannion <mattpmannion@gmail.com>
2021-02-22 17:37:00 -0500

    added a couple models

--- dist/typescript.c99b7304.js
+++ dist/typescript.c99b7304.js
@@ -12330,7 +12330,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55295" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61637" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
